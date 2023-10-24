fa7bccf7e570966afe1341a6c4ba27890824654f
Sandeep Pawar <62612119+pawarbi@users.noreply.github.com>
2023-10-24 12:43:36 -0500

    Update get_dependencies.py

--- get_dependencies.py
+++ get_dependencies.py
@@ -1,3 +1,15 @@
+import pandas as pd
+# import sempy
+import sempy.fabric as fabric
+from sempy.fabric import FabricDataFrame
+# import graphviz
+from graphviz import Digraph
+# import pandas as pd
+import networkx as nx
+from IPython.display import display
+from shlex import quote
+import numpy as np
+
 def get_dependencies(dataset, workspace):
     
 
